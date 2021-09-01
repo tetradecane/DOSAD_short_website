ea7933c5a8d502f590a854da861b2cf2c643a829
Aoyang Yan <xiaoyan9894@sjtu.edu.cn>
2021-09-01 16:45:06 +0800

    README benchmark code

--- code/image_lib.py
+++ code/image_lib.py
@@ -63,5 +63,4 @@ def resize_and_crop(image, target_size=target_size_best, resample_filter=Image.L
 
 
 if __name__ == "__main__":
-    # noinspection PyTypeChecker
-    print(pearson("1.png", "2.png"))
+    pass
