b7c00afa60976d539be29308c10b298a12b2f503
Aoyang Yan <xiaoyan9894@sjtu.edu.cn>
2021-09-01 14:40:09 +0800

    delete files

--- real_bugs/main.py
+++
@@ -1,9 +0,0 @@
-from image_lib import *
-from utils import *
-from params import *
-
-img_paths = traverse_files_with_ext('.', 'png')
-for img_path in img_paths:
-    new_img = resize_and_crop(img_path, target_size=origin_size)
-    new_path = os.path.splitext(img_path)[0] + '_processed.png'
-    new_img.save(new_path)
--- real_bugs/main_delete.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from utils import *
-
-img_paths = traverse_files_with_ext('.', 'png')
-for img_path in img_paths:
-    if img_path.find('_') != -1:
-        print(img_path)
-        os.remove(img_path)
